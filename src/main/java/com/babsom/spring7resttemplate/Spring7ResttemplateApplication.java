722f600e6ef5755d4c8ebf59dc28f8cbd7e49215
Babür Somer <babursomer@gmail.com>
2026-08-18 16:59:28 +0300

    The initial one

--- src/main/java/com/babsom/spring7resttemplate/Spring7ResttemplateApplication.java
+++ src/main/java/com/babsom/spring7resttemplate/Spring7ResttemplateApplication.java
@@ -1,13 +1,12 @@
-package com.babsom.sfgrestbrewery;
+package com.babsom.spring7resttemplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SfgRestfulBreweryApplication {
+public class Spring7ResttemplateApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgRestfulBreweryApplication.class, args);
+		SpringApplication.run(Spring7ResttemplateApplication.class, args);
 	}
-
 }
--- src/main/java/com/babsom/spring7resttemplate/model/BeerStyle.java
+++ src/main/java/com/babsom/spring7resttemplate/model/BeerStyle.java
@@ -1,7 +1,7 @@
-package com.babsom.sfgrestbrewery.domain;
+package com.babsom.spring7resttemplate.model;
 
 
-public enum BeerStyleEnum {
+public enum BeerStyle {
 
     LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
 }
